47bc80b33dd6254a06208b939c297461a0ee8485
Radin <radinmoayyedian@gmail.com>
2025-02-17 21:31:37 -0500

    changed pic, removed airbnb experience

--- src/content/experiences.ts
+++ src/content/experiences.ts
@@ -6,11 +6,11 @@ export const EXPERIENCES = [
     description:
       "Designed gameplay akin to 'Among Us,' where users navigate a plant growing facility, complete tasks, and ensure the plants thrive. Engineered a robust score tracking system, securely storing users’ achievements on AWS servers, and facilitating weekly performance accolades.",
   },
-  {
-    date: "August 2022 - Present",
-    jobTitle: "Co-Host and Cleaner",
-    company: "Airbnb",
-    description:
-      "Managing a unit and ensuring customer satisfaction with a 4.94/5 rating. Communicating with guests and making special arrangements based on their needs. Weekly cleaning guaranteeing safety.",
-  },
+  // {
+  //   date: "August 2022 - Present",
+  //   jobTitle: "Co-Host and Cleaner",
+  //   company: "Airbnb",
+  //   description:
+  //     "Managing a unit and ensuring customer satisfaction with a 4.94/5 rating. Communicating with guests and making special arrangements based on their needs. Weekly cleaning guaranteeing safety.",
+  // },
 ]
